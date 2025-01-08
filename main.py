a56d969f710afd4b5aeed5c613130a08d1a9941d
Vartazian <Vartazian37@gmail.com>
2025-01-08 00:25:58 -0800

    Main now correctly takes words and counts.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+def count_words(text):
+    words = text.split()
+    wordcount = 0
+    for word in words:
+        wordcount += 1
+    return wordcount
+
 def main():
     print("Starting to Read...")
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        wordcount = count_words(file_contents)
+        print(wordcount)
+        #print(file_contents)
 main()
-
-def wordcount(text):
-    words = file_contents.split()
 
